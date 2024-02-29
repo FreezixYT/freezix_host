147682316e680ac453dc918964526fea88548476
freezixYT <freezix.dev@gmail.com>
2024-02-29 11:40:07 +0100

    ajout theme

--- index.php
+++ index.php
@@ -4,52 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FREEZ HOST</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
 <?php
 include 'header.html';
 
-$connecter = false;
-
-if ($connecter == false)
-{
-    include 'co-toi.html';
-}
-else if ($connecter == true)
-{
-    include 'main.html';
-}
-else
-{
-    include 'co-toi.html';
-}
+session_start();
 ?>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div class="slogant">
+    <h2>Un Service d'hébergement de qualité incomparable</h2>
+    <br>
+    <h1>Petit prix, grande performance</h1>
+</div>
+
+<?php
+$connection = true;
+
+// Vérifier l'état de connexion
+if ($connection == false)
+{
+    include 'co-toi.html';
+}
+
+include 'main.html';
+?>
+
 <?php
 include 'footer.html';
 ?>
+
 </body>
 </html>
