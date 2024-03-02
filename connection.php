2e61e1b71167c6c5a8ea2b1861a7bac03e3ee379
freezixYT <freezix.dev@gmail.com>
2024-03-03 00:56:29 +0100

    form connection

--- connection.php
+++ connection.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FREEZ HOST</title>
     <link rel="stylesheet" href="/css/style.css">
 </head>
+
 <body>
 
-<?php
+    <?php
 include 'header.html';
 
 session_start();
 ?>
 
-<h1>Connection</h1>
+    <main>
+        <h2>Connexion</h2>
 
+        <form action="index.php" method="POST">
+            <div class="zone-form">
+                <div class="form-group">
+                    <label for="username">Nom d'utilisateur :</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Mot de passe :</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <div class="form-group">
+                    <button type="submit">Se connecter</button>
+                </div>
+            </div>
+        </form>
+    </main>
 
-<?php
+    <?php
 include 'footer.html';
 ?>
 
 </body>
+
 </html>
