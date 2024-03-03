8ee2656cdd88cc9d4f11d8e992aaec27fde0daad
freezixYT <freezix.dev@gmail.com>
2024-03-03 22:15:38 +0100

    ajout page hebergement

---
+++ Hebergement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FREEZ HOST</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php
+include 'header.html';
+
+session_start();
+?>
+
+<h2>Hebergement</h2>
+
+<p classe="center">Choisiser votre plan idéal ! </p>
+
+<?php
+include 'footer.html';
+?>
+
+</body>
+</html>
